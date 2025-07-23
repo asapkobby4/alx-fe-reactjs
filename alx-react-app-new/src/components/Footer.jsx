4b74eac017a1cea20427692945ebd6f30c60d145
asapkobby4 <asapkoby4@gmail.com>
2025-07-23 10:07:00 -0400

    install react

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style= {{color: 'blue', fontSize: '24px'}}><footer>
+  <p>© 2023 City Lovers</p>
+</footer></div>
+  )
+}
+
+export default Footer

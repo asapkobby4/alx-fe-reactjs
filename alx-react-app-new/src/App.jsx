3fbe94edf6021e240cef9378e993eeaa29810275
asapkobby4 <asapkoby4@gmail.com>
2025-07-23 10:48:55 -0400

    install react

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
         <MainContent />
         <Footer />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <Counter />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

aa0bec7e8072e66a9ba0ad6e726d037f10fd0418
asapkobby4 <asapkoby4@gmail.com>
2025-08-03 18:50:31 -0400

    user search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
